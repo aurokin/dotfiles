a5dfc56e6172f367726f7fb41a5a7052917fe7c0
Hunter Sadler <auro@hsadler.com>
2024-04-21 18:38:01 -0600

    Twigsmux Last Session

--- zsh/.zsh_scripts/twigsmux.sh
+++ zsh/.zsh_scripts/twigsmux.sh
@@ -3,8 +3,12 @@
 tmux_running=$(pgrep tmux)
 tmux_active=$(echo $TMUX)
 is_kill="";
+is_last="";
 if [[ $1 == "k" ]]; then
     is_kill=true;
+fi
+if [[ $1 == "l" ]]; then
+    is_last=true;
 fi
 
 if [[ -z $tmux_active ]]; then
@@ -18,9 +22,20 @@ fi
 if [[ $tmux_running ]]; then
     current_session=$(tmux display-message -p '#S')
 
+    if [[ $is_last ]]; then
+        last_session=$(cat ~/.twigsmux)
+
+        if tmux has-session -t=$last_session > /dev/null; then
+            rm ~/.twigsmux
+            echo $current_session > ~/.twigsmux
+            tmux switch-client -t $last_session
+        fi
+        return;
+    fi
+
     if [[ $current_session != "twigsmux" ]]; then
-        # rm ~/.twigsmux
-        # echo $current_session > ~/.twigsmux
+        rm ~/.twigsmux
+        echo $current_session > ~/.twigsmux
 
         tmux kill-session -t twigsmux;
         tmux new-session -ds twigsmux -c ~
