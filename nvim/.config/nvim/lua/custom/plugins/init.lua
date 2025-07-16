1c553d7219afe5b0ff541232042ae59660bf6b03
Hunter Sadler <auro@hsadler.com>
2025-07-16 13:04:37 -0600

    Remove unused init line

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,12 +2,10 @@ return {
     -- git
     'tpope/vim-fugitive',
     'tpope/vim-rhubarb',
-    -- comment
-    'numToStr/Comment.nvim',
     -- paperplanes
-    "rktjmp/paperplanes.nvim",
+    'rktjmp/paperplanes.nvim',
     -- peekup
-    "gennaro-tedesco/nvim-peekup",
+    'gennaro-tedesco/nvim-peekup',
     -- vim-be-good
-    'ThePrimeagen/vim-be-good'
+    'ThePrimeagen/vim-be-good',
 }
