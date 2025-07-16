0a6e07892ae7eabb72b6656f7295923404515f45
Hunter Sadler <auro@hsadler.com>
2025-07-16 14:07:20 -0600

    Remove useless comments

--- nvim/.config/nvim/lua/custom/plugins/autocomplete.lua
+++ nvim/.config/nvim/lua/custom/plugins/autocomplete.lua
@@ -16,17 +16,7 @@ return { -- Autocompletion
                 end
                 return 'make install_jsregexp'
             end)(),
-            dependencies = {
-                -- `friendly-snippets` contains a variety of premade snippets.
-                --    See the README about individual language/framework/plugin snippets:
-                --    https://github.com/rafamadriz/friendly-snippets
-                -- {
-                --   'rafamadriz/friendly-snippets',
-                --   config = function()
-                --     require('luasnip.loaders.from_vscode').lazy_load()
-                --   end,
-                -- },
-            },
+            dependencies = {},
             opts = {},
         },
         'folke/lazydev.nvim',
