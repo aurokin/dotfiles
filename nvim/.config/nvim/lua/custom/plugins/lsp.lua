23b20990333c9190b1e312afc8448ae0528bc79b
Hunter Sadler <auro@hsadler.com>
2025-07-16 14:04:05 -0600

    Remove cmp

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -27,56 +27,55 @@ return {
     config = function()
         -- [[ Configure LSP ]]
         --  This function gets run when an LSP connects to a particular buffer.
-        local on_attach = function(client, bufnr)
-            -- NOTE: Remember that lua is a real programming language, and as such it is possible
-            -- to define small helper and utility functions so you don't have to repeat yourself
-            -- many times.
-            --
-            -- In this case, we create a function that lets us more easily define mappings specific
-            -- for LSP related items. It sets the mode, buffer and description for us each time.
-            local nmap = function(keys, func, desc)
-                if desc then
-                    desc = 'LSP: ' .. desc
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('auro-lsp-attach', { clear = true }),
+
+            callback = function(event)
+                -- NOTE: Remember that lua is a real programming language, and as such it is possible
+                -- to define small helper and utility functions so you don't have to repeat yourself
+                -- many times.
+                --
+                -- In this case, we create a function that lets us more easily define mappings specific
+                -- for LSP related items. It sets the mode, buffer and description for us each time.
+                local nmap = function(keys, func, desc)
+                    if desc then
+                        desc = 'LSP: ' .. desc
+                    end
+
+                    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
                 end
 
-                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-            end
+                nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+                nmap('<leader>ca', function()
+                    vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
+                end, '[C]ode [A]ction')
 
-            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-            nmap('<leader>ca', function()
-                vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
-            end, '[C]ode [A]ction')
+                nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+                nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+                nmap('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+                nmap('<leader>ld', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+                nmap('<leader>ls', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+                nmap('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-            nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-            nmap('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-            nmap('<leader>ld', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-            nmap('<leader>ls', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            nmap('<leader>lw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                -- Lesser used LSP functionality
+                nmap('<leader>lg', vim.lsp.buf.declaration, '[G]oto Declaration')
+                nmap('<leader>`', vim.lsp.buf.hover, '[L]sp [H]over')
+                nmap('<leader>lh', vim.lsp.buf.signature_help, 'Signature Documentation')
+                nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+                nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+                nmap('<leader>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, '[W]orkspace [L]ist Folders')
 
-            -- Lesser used LSP functionality
-            nmap('<leader>lg', vim.lsp.buf.declaration, '[G]oto Declaration')
-            nmap('<leader>`', vim.lsp.buf.hover, '[L]sp [H]over')
-            nmap('<leader>lh', vim.lsp.buf.signature_help, 'Signature Documentation')
-            nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-            nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-            nmap('<leader>wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, '[W]orkspace [L]ist Folders')
-
-            -- Create a command `:Format` local to the LSP buffer
-            if client.supports_method 'textDcoument/formatting' then
-                vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-                    vim.lsp.buf.format {
-                        tabSize = 4,
-                        insertSpaces = true,
-                        trimTrailingWhitespace = true,
-                        insertFinalNewline = false,
-                        trimFinalNewLines = true,
-                    }
-                end, { desc = 'Format current buffer with LSP' })
-            end
-        end
+                local function client_supports_method(client, method, bufnr)
+                    if vim.fn.has 'nvim-0.11' == 1 then
+                        return client:supports_method(method, bufnr)
+                    else
+                        return client.supports_method(method, { bufnr = bufnr })
+                    end
+                end
+            end,
+        })
 
         require('mason').setup()
         require('mason-lspconfig').setup()
@@ -149,61 +148,8 @@ return {
                 function(server_name)
                     local server = servers[server_name] or {}
                     server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                    server.on_attach = on_attach
                     require('lspconfig')[server_name].setup(server)
                 end,
-            },
-        }
-
-        -- [[ Configure nvim-cmp ]]
-        -- See `:help cmp`
-        local cmp = require 'cmp'
-        local luasnip = require 'luasnip'
-        require('luasnip.loaders.from_vscode').lazy_load()
-        luasnip.config.setup {}
-
-        cmp.setup {
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            completion = {
-                completeopt = 'menu,menuone,noinsert',
-            },
-            mapping = cmp.mapping.preset.insert {
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete {},
-                ['<CR>'] = cmp.mapping.confirm {
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                },
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-            },
-            sources = {
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-                { name = 'path' },
             },
         }
     end,
