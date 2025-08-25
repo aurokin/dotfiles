5174175c8698f92ee7ed6b58ec9772222c7909e2
Hunter Sadler <hsadler@oreillyauto.com>
2025-08-25 16:31:33 -0500

    Remove xcodebuild

--- nvim/.config/nvim/lua/custom/plugins/dap.lua
+++ nvim/.config/nvim/lua/custom/plugins/dap.lua
@@ -1,11 +1,10 @@
 return {
     'mfussenegger/nvim-dap',
     dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "wojciech-kulik/xcodebuild.nvim",
+        'rcarriga/nvim-dap-ui',
         'williamboman/mason.nvim',
         'jay-babu/mason-nvim-dap.nvim',
-        "nvim-neotest/nvim-nio"
+        'nvim-neotest/nvim-nio',
     },
     config = function()
         local dap = require 'dap'
@@ -14,8 +13,7 @@ return {
         require('mason-nvim-dap').setup {
             automatic_setup = true,
             handlers = {},
-            ensure_installed = {
-            },
+            ensure_installed = {},
         }
         vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
         vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
@@ -26,24 +24,10 @@ return {
         vim.keymap.set('n', '<leader>B', function()
             dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
         end, { desc = 'Debug: Set Breakpoint' })
-        dapui.setup({});
+        dapui.setup {}
 
         dap.listeners.after.event_initialized['dapui_config'] = dapui.open
         dap.listeners.before.event_terminated['dapui_config'] = dapui.close
         dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-        local os = vim.env.OS
-        local codellDbDir = vim.env.CODELLDB_DIR;
-        if (os == "darwin" and type(codellDbDir) == "string" and codellDbDir ~= "") then
-            local xcodebuild = require("xcodebuild.integrations.dap")
-            local codelldbPath = string.format("%s%s", codellDbDir, "/extension/adapter/codelldb")
-            xcodebuild.setup(codelldbPath)
-
-            vim.keymap.set("n", "<leader>dd", xcodebuild.build_and_debug, { desc = "Build & Debug" })
-            vim.keymap.set("n", "<leader>dr", xcodebuild.debug_without_build, { desc = "Debug Without Building" })
-            vim.keymap.set("n", "<leader>dt", xcodebuild.debug_tests, { desc = "Debug Tests" })
-            vim.keymap.set("n", "<leader>dT", xcodebuild.debug_class_tests, { desc = "Debug Class Tests" })
-            vim.keymap.set("n", "<leader>dx", xcodebuild.terminate_session, { desc = "Terminate Debugger" })
-        end
-    end
+    end,
 }
