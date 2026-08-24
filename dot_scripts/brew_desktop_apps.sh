9d4660fcc6e98878b31708c9d272415e045cce3f
Hunter Sadler <auro@hsadler.com>
2026-08-24 15:52:08 -0600

    Remove manually managed desktop apps

--- dot_scripts/brew_desktop_apps.sh
+++ dot_scripts/brew_desktop_apps.sh
@@ -4,8 +4,6 @@ brew install --cask android-platform-tools
 brew install --cask balenaetcher
 brew install --cask ghostty
 brew install --cask kitty
-brew install --cask codex-app
-brew install --cask github-copilot-app
 brew install --cask t3-code
 brew install --cask font-monaspace-nerd-font
 brew install --cask font-monaspice-nerd-font
@@ -21,7 +19,6 @@ brew install --cask moonlight
 brew install --cask openmtp
 brew install --cask steam
 brew install --cask retroarch
-brew install --cask the-unarchiver
 # Laptop-only: brew install --cask caffeine
 brew install --cask docker-desktop
 brew install --cask zen
